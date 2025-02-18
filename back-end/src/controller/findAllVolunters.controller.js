77ea2d3c1f2910d2ee00f3d4aa7439cbb1ed7410
Andre Nascimento <1914andre@gmail.com>
2025-02-18 14:19:04 -0300

    fix: add alteration to initial structure in findAll volunteers

--- back-end/src/controller/findAllVolunters.controller.js
+++ back-end/src/controller/findAllVolunters.controller.js
@@ -1,9 +1,12 @@
-import { VoluntersToCart } from "../model/model.js";
+import { VoluntersToCart } from "../model/voluntersToCart.js"
 
 const findAllVolunter = (req,res) => {
     try {
-        
+        const getAllVolunters = VoluntersToCart.findAll()
+        res.status(200).send(getAllVolunters)
     } catch (error) {
-        
+        res.status(500).send(error)     
     }
 }
+
+export{findAllVolunter}
