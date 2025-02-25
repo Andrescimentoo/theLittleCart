d0a7e2d7f98ceec526ffb2787477aa27e69276d4
Andre Nascimento <1914andre@gmail.com>
2025-02-25 17:38:41 -0300

    feat: separing the two tables of the model

--- back-end/src/model/voluntersToCart.js
+++ back-end/src/model/voluntersToCart.js
@@ -1,44 +1,40 @@
 import { dataBase } from "../database/config.js";
 import { DataTypes } from "sequelize";
 
-const VoluntersToCart = dataBase.define('VoluntersToCart', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-   
-    name: {
-      type: DataTypes.STRING(200),
-      allowNull: false
-    },
-    
-    last_name: {
-      type: DataTypes.STRING(200),
-      allowNull: false
-    },
-    
-    availableHours: {
-      type: DataTypes.STRING(200),  // Ou TEXT, se precisar de mais flexibilidade
-      allowNull: true
-    },
-    
-    availableDays: {
-      type: DataTypes.STRING(200),  
-      allowNull: true
-    },
-    
-    fixedDays: {
-      type: DataTypes.STRING(200),  
-      allowNull: true
-    },
-   
-    status: {
-      type: DataTypes.STRING(100),  
-      allowNull: true
-    }
-  }, {
-    timestamps: false 
-  });
-  
-  export{VoluntersToCart}
+
+const VoluntersToCart = dataBase.define("VoluntersToCart", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+
+  fullName: {
+    type: DataTypes.STRING(200),
+    allowNull: false,
+  },
+
+  availableStartTime: {
+    type: DataTypes.TIME,
+    allowNull: false,
+  },
+
+  availableEndTime: {
+    type: DataTypes.TIME,
+    allowNull: false,
+  },
+
+  availableDaysOfTheWeek: {
+    type: DataTypes.STRING(200),
+    allowNull: false,
+  },
+
+  fixedDays: {
+    type: DataTypes.STRING(200), // "sim" ou NULL
+    allowNull: true,
+  },
+});
+
+
+
+export {VoluntersToCart} ;
