e4d6d6eaab4cb9d2117702370028c6d0bf595a3a
Andre Nascimento <1914andre@gmail.com>
2025-02-25 17:35:28 -0300

    fix: removing initial structure to findAll volunters

--- back-end/src/controller/findAllVolunters.controller.js
+++ back-end/src/controller/findAllVolunters.controller.js
@@ -1,12 +1,9 @@
-import { VoluntersToCart } from "../model/voluntersToCart.js"
 
-const findAllVolunter = (req,res) => {
-    try {
-        const getAllVolunters = VoluntersToCart.findAll()
-        res.status(200).send(getAllVolunters)
-    } catch (error) {
-        res.status(500).send(error)     
-    }
-}
 
-export{findAllVolunter}
+
+
+
+
+
+
+
